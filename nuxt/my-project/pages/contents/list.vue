63ff17a6c045ab68860ac8f0923441c219d75617
heejunghwang <gyahoo617@gmail.com>
2017-08-30 18:25:46 +0900

    next 기본 예제 생성

---
+++ nuxt/my-project/pages/contents/list.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        테스트
+      </h1>
+      <h2 class="subtitle">
+        리스트 페이지
+      </h2>
+      <table class="table">
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+        </tbody>
+      </table>
+
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
